d5abc5edefec3aef60c2ae921c96ec5c8044e50f
Oliver O'Toole <oo287@cam.ac.uk>
2021-11-09 10:52:36 +0000

    Added Servo, DC Motor and Ultrasonic sensor files

--- Control_Sweep_final/Control_Sweep_final.ino
+++ Control_Sweep_final/Control_Sweep_final.ino
@@ -13,12 +13,18 @@ Servo myservo;  // create servo object to control a servo
 // twelve servo objects can be created on most boards
 
 int pos = 0;    // variable to store the servo position
+int angle;
+
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  Serial.begin(9600);
 }
 
 void loop() {
+  if (Serial.available()) {
+    angle = Serial.parseInt();
+  }
   for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
