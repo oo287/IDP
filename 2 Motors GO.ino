24e3cbfd34423bdd43c760e8f16881b70f1ee397
dk617 <77797663+dk617@users.noreply.github.com>
2021-11-09 12:29:41 +0000

    Add files via upload
    Arduino code for two motors turning at the same time with speed = 100. Note that motor ports 3 and 4 are used

---
+++ 2 Motors GO.ino
@@ -0,0 +1,52 @@
+/*
+This is a test sketch for the Adafruit assembled Motor Shield for Arduino v2
+It won't work with v1.x motor shields! Only for the v2's with built in PWM
+control
+
+For use with the Adafruit Motor Shield v2
+---->	http://www.adafruit.com/products/1438
+*/
+
+#include <Adafruit_MotorShield.h>
+
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
+
+// Select which 'port' M1, M2, M3 or M4. In this case, M3
+Adafruit_DCMotor *myMotor = AFMS.getMotor(3);
+// You can also make another motor on port M4
+Adafruit_DCMotor *myOtherMotor = AFMS.getMotor(4);
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+  if (!AFMS.begin()) {         // create with the default frequency 1.6KHz
+  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+    Serial.println("Could not find Motor Shield. Check wiring.");
+    while (1);
+  }
+  Serial.println("Motor Shield found.");
+
+  // Set the speed to start, from 0 (off) to 255 (max speed)
+  myMotor->setSpeed(150);
+  myMotor->run(FORWARD);
+  // turn on motor
+  myMotor->run(RELEASE);
+
+  myOtherMotor->setSpeed(150);
+  myOtherMotor->run(FORWARD);
+  // turn on motor
+  myOtherMotor->run(RELEASE);
+}
+
+void loop() {
+  int DCspeed = 100;
+
+  myMotor->run(FORWARD);
+  myMotor->setSpeed(DCspeed);
+  myOtherMotor->run(FORWARD);
+  myOtherMotor->setSpeed(DCspeed);
+}
