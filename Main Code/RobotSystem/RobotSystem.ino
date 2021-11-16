2975c7288d9126e773e9a52913e0e2cbf9f70baa
oo287 <77851965+oo287@users.noreply.github.com>
2021-11-16 11:36:36 +0000

    Update RobotSystem.ino

--- Main Code/RobotSystem/RobotSystem.ino
+++ Main Code/RobotSystem/RobotSystem.ino
@@ -6,7 +6,9 @@ byte robot_test_state = 5;                                       // Variable to 
 unsigned long tick_counter = 0;                                  // Counts the number of ticks elapsed since program started running
 unsigned const int tick_length = 5;                              // The length of one tick in milliseconds. Clock Frequency = 1/tick_length
 unsigned long time_when_i_say_so = 0;
-
+bool horizontal_line = false
+bool finished_dropping = false
+bool picked_up_yet = false
 byte robot_state = 0;                                            // Variable to track the stage of the problem (0=start,1=got 1 dummy,2=dropped off one dummy)
 
 bool line_detector_1, line_detector_2, line_detector_3;          // Booleans to describe if each line sensor has a line below or not
@@ -16,15 +18,14 @@ int min_IR_sensor_magnitude;                                     // Minimum valu
 int US_magnitude;                                                // Distance recorded by the ultrasonic sensor. Return 200 if out of range (dist > 200)
 
 int what_dummy_am_I;                                             // The dummy that is detected (0 for line, 1 for red box, 2 for blue box)
-bool horizontal_line;                                            // Variable to track if 
 
 const byte left_motor_port =  3;                                 // Motor shield port that the left motor uses
 const byte right_motor_port = 4;                                 // Motor shield port that the right motor uses
 
 const byte LED1_PIN = 6;                                         // Pin used for Orange LED (LED1)
-const byte LS1_PIN = A2;                                         // Pins used for Line Sensors (LS) 1, 2 and 3
-const byte LS2_PIN = A1;
-const byte LS3_PIN = A0;
+const byte LS1_PIN = 2;                                          // Pins used for Line Sensors (LS) 1, 2 and 3
+const byte LS2_PIN = 1;
+const byte LS3_PIN = 0;
 
 // --------- Motor Initialisation ---------                      // --------- Motor Initialisation ---------
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();              // Create motorshield object called 'AFMS' with default I2C address. Put I2C address in brackets if different address needed
@@ -55,16 +56,38 @@ void drive_motor(Adafruit_DCMotor* motor, int spd, bool rev) {   // Electrical f
   } 
 }
 
-void take_IR_sensor_reading() {                                 // Electrical function that updates the value of IR_sensor_magnitude
-
-  // ELECTRICAL WRITE THIS FUNCTION (PLEASE)
-
+int take_IR_sensor_reading(int IR_PIN=a5, int samples=107) {
+  // Function to record voltage amplitude of IR sensor 10% duty cycle signal
+  // IR_PIN must be an analog pin for any non-binary results.
+  // Wave is ~6ms long, and samples take ~ 112us, so set samples to AT LEAST >54 to ensure peak is included in measurement (preferably > 108)
+  // CODE IS BLOCKING TO ALLOW FAST SAMPLING RATE - blocking time will increase linearly with samples
+  
+  int max_val;                            // Variable to store max. voltage measured
+  int new_val;                            // Variable to store current measurement
+  for (int i=0; i<samples; i++) {
+    new_val = analogRead(IR_PIN);         // Measure current voltage from photodiode
+    if (new_val > max_val) {
+      max_val = new_val;                  // Update maximum recorded value if it is new highest
+    }
+  }
+  return max_val;
 }
 
-int take_ultrasonic_reading() {                                  // Electrical function that updates the value of the ultrasonic
-
-  // ELECTRICAL WRITE THIS FUNCTION (PLEASE)
-  
+int take_ultrasonic_reading() {
+  long duration, distance;                                    //no idea how this works but should just return distance, add some prints if i broke it
+  digitalWrite(trigPin, LOW);  // Added this line 
+  delayMicroseconds(2); // Added this line 
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10); // Added this line 
+  digitalWrite(trigPin, LOW); 
+  duration = pulseIn(echoPin, HIGH); 
+  distance = (duration/2) / 29.1; 
+  if (distance >= 200 || distance <= 0){
+    return 200;
+  } 
+  else { 
+    return distance;
+  } 
 }
 
 void take_line_sensor_readings() {
@@ -87,70 +110,42 @@ void take_line_sensor_readings() {
   else {
     line_detector_3 = false;
   }
-//  
-//  if (digitalRead(LS1_PIN)==1) {
-//    line_detector_1 = true;
-//  }
-//  else {
-//    line_detector_1 = false;
-//  }
-//  if (digitalRead(LS2_PIN)==1) {
-//    line_detector_2 = true;
-//  }
-//  else {
-//    line_detector_2 = false;
-//  }
-//  if (digitalRead(LS3_PIN)==1) {
-//    line_detector_3 = true;
-//  }
-//  else {
-//    line_detector_3 = false;
-//  }
-  
-}
-
-void drive_servo(byte servo_number, int servo_angle) {           // Electrical function that sets the servo angle to the stated value. 
-
-  // ELECTRICAL WRITE THIS FUNCTION (PLEASE)
-  
 }
 
 // --------- Software Functions ---------                        // --------- Software Functions ---------
 void follow_line() {                                             // Function that drives the motors and uses line sensors to move allow the line. Doesn't take inputs to stop (only call this function if the path is clear) ouput true when hits horizontal line
-  take_line_sensor_readings();
+  take_line_sensor_readings();                                   // Update line sensor booleans
                                                                  // Default on the line, go straight ahead case
   if ((line_detector_2) and (not line_detector_1) and (not line_detector_3)){    
-    drive_motor(left_motor_port, 255, false);
-    drive_motor(right_motor_port, 255, false);
+    drive_motor(left_motor, 255, false);
+    drive_motor(right_motor, 255, false);
     Serial.println("1");
   }                                                              // Central detector off line but niether side on line yet but carry on straight (this shouldn't happen normally)
   else if ((not line_detector_2) and (not line_detector_1) and (not line_detector_3)){ 
-    drive_motor(left_motor_port, 200, false);
-    drive_motor(right_motor_port, 200, false);
+    drive_motor(left_motor, 200, false);
+    drive_motor(right_motor, 200, false);
     Serial.println("2");
   }                                                              // Hit line on LHS  so steering RIGHT
   else if ((line_detector_1) and (not line_detector_3)){ 
-    drive_motor(left_motor_port, 255, false);
-    drive_motor(right_motor_port, 200, false);
+    drive_motor(left_motor, 255, false);
+    drive_motor(right_motor, 200, false);
     Serial.println("3");
   }                                                              // Hit line on RHS  so steering LEFT
   else if ((not line_detector_1) and (line_detector_3)){ 
-    drive_motor(left_motor_port, 200, false);
-    drive_motor(right_motor_port, 255, false);
+    drive_motor(left_motor, 200, false);
+    drive_motor(right_motor, 255, false);
     Serial.println("4");
   }                                                              // Hit horizontal line (return TRUE)
   else if ((line_detector_2) and (line_detector_1) and (line_detector_3)){ 
-    drive_motor(left_motor_port, 0, false);
-    drive_motor(right_motor_port, 0, false);
+    drive_motor(left_motor, 0, false);
+    drive_motor(right_motor, 0, false);
     Serial.println("5");
-    return true;
   }
   else{                                                          // Some funky angles going on here, not an ideal case just sorta spin a'c'wise I guess
-    drive_motor(right_motor_port, 100, false);
-    drive_motor(left_motor_port, 100, true);
+    drive_motor(right_motor, 50, false);
+    drive_motor(left_motor, 50, true);
     Serial.println("6");
   }
-  return false;
 }
 
 //bool point_towards_nearest_dummy(bool clockwise=true){           // Function to: Stop, spin on the spot, turn towards first dummy seen (moving in the direction given)
@@ -172,19 +167,19 @@ void follow_line() {                                             // Function tha
 //  // UNFINISHED (OLLIE)
 //}
 
-void identify_dummy(){                                           // Reads IR input signal and determines which dummy is in front of it 
+int identify_dummy(){                                           // Reads IR input signal and determines which dummy is in front of it 
   // write this when electrical sorted ir sensor
 }
 
 bool pick_up_dummy(){
-  // when claw built write this and give output of when its 
+  // when claw built write this and give output of when its return true when done
 }
 
-void drop_off_dummy(){
-  // write this when claw built
+bool drop_off_dummy(){
+  // write this when claw built return true when done
 }
 void approach_identify_grab(){
-
+  //idk if we need this but its here if we do
 }
 // --------- Setup Function ---------                            // --------- Setup Function ---------
 void setup() {                                                   // Function that runs on power-up/RESET
@@ -200,7 +195,11 @@ void setup() {                                                   // Function tha
   pinMode(LS1_PIN, INPUT);                                       // Declare the line sensor pins as inputs
   pinMode(LS2_PIN, INPUT);
   pinMode(LS3_PIN, INPUT);
-  
+
+  pinMode(trigPin, OUTPUT);                                       //Declare pins for ultrasound sensor
+  pinMode(echoPin, INPUT); 
+
+
   robot_state = 0;                                               // Reset robot state to default
 
   left_motor->setSpeed(0);                                       // Initialise left motor
@@ -251,10 +250,77 @@ void loop() {                                                    // Function tha
   }
   else if (robot_test_state == 5) {
     if (tick_counter*tick_length > 3000) {
-      horizontal_line = follow_line();
+      follow_line();
     }
   }
-  
+/*  else if (tick_length * tick_counter > 3000) {                    // main program
+   if (robot_state == 0){                                        // starting sequence -> pick up first dummy
+     if ((take_ultrasonic_reading() > 5) and (take_IR_sensor_readings() < 1024)){                        // this should drive us up over the ramp to the first dummy no idea what the ir value should be right now
+       follow_line();
+     }
+     else {
+       drive_motor(left_motor,0,true);                                    
+       drive_motor(right_motor,0,true);
+       if (time_when_i_say_so == 0){
+         time_when_i_say_so = tick_counter*tick_length;
+       }
+       if (tick_length * tick_counter > time_when_i_say_so + 5000){
+         which_dummy_am_I = identify_dummy();
+         picked_up_yet = pick_up_dummy();
+       }
+       if (picked_up_yet == true){
+         time_when_i_say_so = 0;
+         robot_state = 1;
+       }
+     }
+   }
+ 
+    if (robot_state == 1){      //picked up first dummy and drop it off
+      if (not follow_line()){
+        horizontal_line = follow_line()
+      }
+      if (horizontal_line){
+        finished_dropping = drop_off_dummy();
+      }
+      if (finished_dropping == true){
+        robot_state == 2;
+        finished_dropping = false
+      }
+    }
+    if (robot_test_state ==6){      //first competetion breakaway point RESUME FROM HERE
+    }
+    else{
+    if (robot_state == 2){     // back out of drop off
+      if (time_when_i_say_so == 0){
+        time_when_i_say_so = tick_counter * tick_length;
+      }
+      else if (tick_counter * tick_length < time_when_i_say_so + 1000){
+        drive_motor(left_motor,255,true);
+        drive_motor(right_motor,255,true);
+      }
+      else{
+       robot_state = 3;
+      }
+    }
+   
+   if (robot_state == 3){                                       //find 2nd dummy
+     point_towards_nearest_dummy();
+     if ((take_ultrasonic_reading() > 5) and (take_IR_sensor_readings() < 1024)){                        // this should drive us to the dummy
+       drive_motor(left_motor,255,false);
+       drive_motor(right_motor,255,false);
+       pick_up_dummy()
+       if ((not line_detector_1) and (not line_detector_2) and (not line_detector_3)){
+         drive_motor(left_motor,255,true);
+         drive_motor(right_motor,255,true);
+       }
+       else{
+       
+       }
+     }
+   } 
+ }
+ } */
+
   tick_counter ++;                                               // Increment tick counter
   delay(tick_length - (millis() % tick_length));                 // Delay the remaining milliseconds of the tick to keep tick rate constant (as long as computer fast enough)
 }
